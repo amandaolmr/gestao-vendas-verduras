257f5720c3a6dcf84d1931e7aaf991dd864cbbd4
Amanda Miranda <amanda.miranda@ntconsult.com.br>
2026-04-27 08:56:21 -0300

    chore: formatação automática

--- vite.config.ts
+++ vite.config.ts
@@ -9,10 +9,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   vite: {
     preview: {
-      allowedHosts: [
-        "gestao-vendas-verduras.onrender.com",
-        ".onrender.com"
-      ]
-    }
-  }
+      allowedHosts: ["gestao-vendas-verduras.onrender.com", ".onrender.com"],
+    },
+  },
 });
