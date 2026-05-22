008fceca7d78dab749cacddeb7fb116b16e06ce8
Amanda Miranda <17412090+amandaolmr@users.noreply.github.com>
2026-05-22 09:04:39 -0300

    commit

--- supabase/migrations/20260424120159_2b20405d-854e-46bd-acd4-ca45e0971fd7.sql
+++ supabase/migrations/20260424120159_2b20405d-854e-46bd-acd4-ca45e0971fd7.sql
@@ -1,10 +1,10 @@
 
--- Função utilitária de timestamps
+-- Função utilitária de timestamps teste
 CREATE OR REPLACE FUNCTION public.update_updated_at_column()
 RETURNS TRIGGER AS $$
 BEGIN
   NEW.updated_at = now();
-  RETURN NEW;
+  RETURN NEW;  
 END;
 $$ LANGUAGE plpgsql SET search_path = public;
 
